d4198a1a7c872861710a406a38fe210461d64224
juan <juancvillefer@gmail.com>
2024-10-30 16:10:13 -0400

    update new dockerfile and run-httpd.sh

--- docker-freepbx/run-httpd.sh
+++ docker-freepbx/run-httpd.sh
@@ -7,9 +7,10 @@ if [ ! -f /var/www/html/.pbx ]; then
   /usr/sbin/safe_asterisk
   # wait for MariaDB
   /wait-for-mariadb.sh
-  # install freepbx
+  # download and install freepbx
+  wget -O /usr/local/src/freepbx-17.0-latest-EDGE.tgz http://mirror.freepbx.org/modules/packages/freepbx/freepbx-17.0-latest-EDGE.tgz
+  tar zxvf /usr/local/src/freepbx-17.0-latest-EDGE.tgz -C /usr/local/src
   cd ${WORKDIR}
-  sleep 5
   ./install --dbengine=${DBENGINE} --dbname=${DBNAME} --dbhost=${DBHOST} --dbport=${DBPORT} \
   --cdrdbname=${CDRDBNAME} --dbuser=${DBUSER} --dbpass=${DBPASS} --user=${USER}  --group=${GROUP} \
   --webroot=${WEBROOT} --astetcdir=${ASTETCDIR} --astmoddir=${ASTMODDIR} --astvarlibdir=${ASTVARLIBDIR} \
